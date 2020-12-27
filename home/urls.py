dc81ac7688fe65cee5e0333544b0b812e1395796
abhishek kushwahha <abhishekushwaha610@users.noreply.github.com>
2020-12-27 23:23:37 +0530

    creates contactus

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
 from django.views.generic import TemplateView
 urlpatterns = [
-    path("",TemplateView.as_view(template_name="home.html"))
+    path("",TemplateView.as_view(template_name="home.html")),
+    path("contact-us/",TemplateView.as_view(template_name="contact-us.html"),name="contact-us"),
+    path("about-us/",TemplateView.as_view(template_name="about-us.html"),name="about-us")
 ]
