55f527fc765c68cd499568e3a0f5730c90836f29
deepak9039 <61735881+deepak9039@users.noreply.github.com>
2021-01-02 23:27:27 +0530

    service page done

--- gearuplead/urls.py
+++ gearuplead/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("home.urls")),
+    path("services",include("services.urls")),
     path('tinymce/', include('tinymce.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
