d3f720fab097e8e31022cbd93044497524aa1bc0
abhishek kushwahha <abhishekushwaha610@users.noreply.github.com>
2021-01-07 01:52:40 +0530

    makinfg service dnamic
    makimigrations and migrate it

--- services/models.py
+++ services/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from tinymce.models import HTMLField
+# from tinymce import TinyMCE
+class Service(models.Model):
+    heading = models.CharField(max_length=100)
+    sub_heading = models.CharField(max_length=100)
 
-# Create your models here.
+    def __str__(self):
+        return self.heading
+
+class ServiceSection(models.Model):
+    parent = models.ForeignKey(Service,on_delete=models.CASCADE)
+    heading = models.CharField(max_length=100)
+    content = HTMLField()
+    image = models.FileField(upload_to="service/")
+    
--- services/views.py
+++ services/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,HttpResponse
-
+from .models import Service
 # Create your views here.
 
 
 def services(request):
-    return render(request,'service.html')
+    
+    return render(request,'service.html',{
+        "service" : Service.objects.first()
+    })
