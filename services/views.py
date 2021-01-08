55f64e11a5fd69a45c99f1180842a4a845ef1f02
abhishek kushwahha <abhishekushwaha610@users.noreply.github.com>
2021-01-09 00:18:21 +0530

    Cleaning Pages

--- services/views.py
+++ services/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,HttpResponse
 from .models import Service
 # Create your views here.
-
+from home.models import Our_client
 
 def services(request):
+    section5 = Our_client.objects.first()
     
     return render(request,'service.html',{
-        "service" : Service.objects.first()
+        "service" : Service.objects.first(),
+        "section5" : section5
     })
